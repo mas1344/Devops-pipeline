42d1b011344cf216512f50ccff1ce831920bade2
Katrin katryl-yh <katrin@rylander.eu>
2025-11-10 14:40:38 +0100

    add: terraform infrastructure as code

---
+++ terraform/01-infrastructure/variables.tf
@@ -0,0 +1,41 @@
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "docker_image" {
+  description = "Docker image name"
+  type        = string
+  default     = "katrinrylander/crypto-dashboard"
+}
+
+variable "docker_image_tag" {
+  description = "Docker image tag"
+  type        = string
+  default     = "latest"
+}
+
+variable "coinmarketcap_api_key" {
+  description = "CoinMarketCap API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "coinmarketcap_symbols" {
+  description = "Cryptocurrency symbols to track"
+  type        = string
+  default     = "BTC"
+}
+
+variable "update_freq_sec" {
+  description = "Update frequency in seconds"
+  type        = string
+  default     = "60"
+}
