e9ef015a2fa54b53345b4f6ba3722a74b196a07f
Maryam Moghadasi <moghadasi.64@gmail.com>
2025-11-13 20:53:12 +0100

    constant file is upfdated

--- src/webapp/constants.py
+++ src/webapp/constants.py
@@ -7,6 +7,7 @@ dotenv.load_dotenv()
 BASE_URL = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest"
 
 COINMARKETCAP_API_KEY = os.getenv("COINMARKETCAP_API_KEY", "")
+print("🔑 API KEY length:", len(COINMARKETCAP_API_KEY) if COINMARKETCAP_API_KEY else None)
 COINMARKETCAP_SYMBOLS = os.getenv("COINMARKETCAP_SYMBOLS", "BTC")
 
 UPDATE_FREQ_SEC = int(os.getenv("UPDATE_FREQ_SEC", 60))
